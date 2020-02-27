0a7a3ba3d35ea079a8d0d6af8d7d99855eabbbc7
Ardy Armando <ardybelian@gmail.com>
2020-02-27 23:42:21 +0700

    test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  title='Rengko Panusunan Malau';
+  nama='Rengko Panusunan Malau';
+  kelas='IF 4 Batamindo';
+  nim='3311811031';
+  prodi='Teknik Informatika'
+  spp='5500000';
+  a=7;
+  b=2;
+  c=this.a+this.b;
+  
+  angka1;
+  angka2;
+  hasil=0;
   constructor() {}
+
+  kali(){
+    this.hasil = this.angka1*this.angka2
+
+  }
+  kurang(){
+    this.hasil = this.angka1-this.angka2
+
+  }
+  tambah(){
+    this.hasil = this.angka1+this.angka2
+
+  }
+  bagi(){
+    this.hasil = this.angka1/this.angka2
+
+  }
 
 }
